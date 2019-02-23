bc55dd3f173ef5fdd47cb552cad767b1d85b5c58
garyandtaz321 <16625600+garyandtaz321@users.noreply.github.com>
2019-02-23 17:38:04 -0500

    Update firebaseFunctions.js

--- functions/firebaseFunctions.js
+++ functions/firebaseFunctions.js
@@ -3,3 +3,23 @@ import db from "../config/firebase";
 var firebaseFunctions = function(){
 
 }
+
+firebaseFunctions.prototype = {
+
+    createUser(dataBlock){
+        db.collection("users").add({
+            age: dataBlock.age,
+            nameFirst: dataBlock.nameFirst,
+            nameLast: dataBlock.nameLast,
+            email: dataBlock.email,
+            zipCode: dataBlock.zipCode,
+            password: dataBlock.password
+
+        }).then(function(docRef) {
+            console.log("Created user in firebase Storage with ID: ", docRef.id);
+        }).catch(function(error){
+            console.log("User creation function threw error: ", error);
+        })
+    }
+
+}
