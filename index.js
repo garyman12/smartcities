202818d993c23ed5bd41c10fa9e4b296c4d639ed
garyandtaz321 <16625600+garyandtaz321@users.noreply.github.com>
2019-02-23 18:49:22 -0500

    a
    a

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 var jwtFunctions = require('./functions/jwtFunctions.js')
+var session = require('express-session')
 const app = express()
 
 
 app.use(bodyParser.json())        
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.send("Hello World!") // Redirect to login page with Github, Google and Facebook Oauth2 logins
+
+app.post('/login', (req,res) =>{
+
 });
 
-app.get('/login', (req,res) =>{
+app.post('/createRequest' , (req,res) =>{
 
-})
+});
+
+app.post('/createRequest' , (req,res) =>{
+
+});
 
 
 app.listen(3001, () => console.log('Server Started on port 3000'))
