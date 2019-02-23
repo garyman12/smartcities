a8e92a8f640c59ca02628451e33ec53a0a656c2a
garyandtaz321 <16625600+garyandtaz321@users.noreply.github.com>
2019-02-23 18:40:12 -0500

    a
    a

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send("Hello World!") // Redirect to login page with Github, Google and Facebook Oauth2 logins
-    });
+});
+
+app.get('/login', (req,res) =>{
+
+})
 
 
-app.listen(3000, () => console.log('Server Started on port 3000'))
+app.listen(3001, () => console.log('Server Started on port 3000'))
