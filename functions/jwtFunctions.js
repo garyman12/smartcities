ef68f75af8a46d9a634d574caa7508cd4b44fe1d
garyandtaz321 <16625600+garyandtaz321@users.noreply.github.com>
2019-02-23 18:23:41 -0500

    c
    a

---
+++ functions/jwtFunctions.js
@@ -0,0 +1,38 @@
+var key = "asdasfkajosbdhaisjlfakspkfljnbuqhip2ow3asdjnasl"
+var jwt = require('jsonwebtoken');
+var jwtFunctions = function(signKey){
+key = signKey // Assign our generated Secret.
+}
+
+jwtFunctions.prototype = {
+
+    createToken(data){
+        return new Promise(function(fulfill , reject){
+            jwt.sign(data, key, { expiresIn: '1h' }, function(error, token){
+                if(error){
+                    reject(error)
+                }else{
+                    fulfill(token)
+                console.log("Token Created!");
+                }
+            });
+            
+        })
+
+    },
+    getPayload(token){
+        return new Promise(function(fulfill, reject){
+            jwt.verify(token, key, function(error, decodedPayload){
+                if(error){
+                    reject(error)
+                }else{
+                    fulfill(decodedPayload)
+                }
+            })
+        })
+    }
+
+
+
+}
+module.exports = jwtFunctions
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+var jwtFunctions = require('./functions/jwtFunctions.js')
 const app = express()
 
 
