956523a6a47889763e017ec6c70891ebc64b65bd
Nathan <nathan.2002.rogers@gmail.com>
2019-02-24 00:33:32 -0500

    Importing titleboard

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../css/dashboard.css";
-import { slide as Menu } from 'react-burger-menu';
+import Titleboard from "./titleboard";
 
 import axios from "axios";
 
@@ -31,19 +31,14 @@ class Dashboard extends Component {
     });
   }
 
-  showSettings (event) {
+  showSettings(event) {
     event.preventDefault();
   }
 
   render() {
     return (
       <div className="App">
-        <Menu>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="about" className="menu-item" href="/about">About</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a>
-          <a onClick={ this.showSettings } className="menu-item--small" href="https://google.com">Settings</a>
-        </Menu>
+        <Titleboard />
         <h1>h</h1>
       </div>
     );
