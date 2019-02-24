13a269362356a16dc18980c8d7eba0aa4ca52016
garyandtaz321 <16625600+garyandtaz321@users.noreply.github.com>
2019-02-23 19:13:10 -0500

    a
    a

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const express = require('express')
 var bodyParser = require('body-parser')
 var jwtFunctions = require('./functions/jwtFunctions.js')
 var session = require('express-session')
+const uuidv4 = require('uuid/v4')
 const app = express()
 
 
 app.use(bodyParser.json())        
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({ secret: uuidv4(), cookie: { maxAge: 60000 }}))
 
 
 app.post('/login', (req,res) =>{
-
+    console.log("POST")
 });
 
 app.post('/createRequest' , (req,res) =>{
 
 });
 
-app.post('/createRequest' , (req,res) =>{
+app.post('/register' , (req,res) =>{
 
 });
 
