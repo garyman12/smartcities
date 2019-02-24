93154846911bf9fe43e7e929296ab1033b858ed5
garyandtaz321 <16625600+garyandtaz321@users.noreply.github.com>
2019-02-24 10:08:35 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,6 @@ const Content = () => (
     <Route exact path="/titleboard" component={Titleboard} />
     <Route exact path="/createRequest" component={CreateRequest} />
     <Route exact path="/postDetails" component={PostDetails} />
-    <Route exact path="/maps" component={Maps} />
   </Switch>
 );
 
