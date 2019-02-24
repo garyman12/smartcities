83d674084e271b6c5d3e5ceced7e6a9f1d9e47ed
Nathan <nathan.2002.rogers@gmail.com>
2019-02-24 10:59:57 -0500

    Added api key

--- client/src/components/maps.js
+++ client/src/components/maps.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
 import axios from "axios";
 
- 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+
 class Maps extends Component {
   static defaultProps = {
     center: {
@@ -23,24 +22,25 @@ class Maps extends Component {
       console.log(res);
     });
   }
- 
+
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '40vh', width: '100%'}}>
+      <div style={{ height: "40vh", width: "100%" }}>
         <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyATxm8bDLqjEVPHa4X0gaRYUGjXlrKu0bI" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
           <AnyReactComponent
             lat={41.6538624}
             lng={-83.62065919999999}
-            text={'Problem Location'}
+            text={"Problem Location"}
           />
         </GoogleMapReact>
       </div>
     );
   }
 }
- 
+
 export default Maps;
