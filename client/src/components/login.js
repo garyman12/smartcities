0de375030a9a022b9eba5f2be989aaf8db27a326
jakkuthompson <jakkuthompson@gmail.com>
2019-02-23 20:41:23 -0500

    login page on fleek

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "../css/login.css";
+import "../css/main.css";
+import "../css/util.css";
 import axios from "axios";
 
 class Login extends Component {
@@ -32,52 +34,64 @@ class Login extends Component {
   render() {
     return (
       <div className="App">
-        <div className="outerCenter">
-          <div className="middleCenter">
-            <div className="innerCenter">
-              <form onSubmit={this.onSubmit}>
-                <div className="">
-                  <span className="">Email</span>
-                </div>
-                <div className="" data-validate="Email is required">
-                  <input
-                    className=""
+        <div className="limiter">
+		      <div className="container-login100">
+			      <div className="wrap-login100">
+				      <form onSubmit={this.onSubmit} className="login100-form validate-form">
+					      <span className="login100-form-title p-b-26">
+						      Welcome
+					      </span>
+					      <span className="login100-form-title p-b-48">
+						      <i className="zmdi zmdi-font"></i>
+					      </span>
+
+					      <div className="wrap-input100 validate-input" data-validate="Email is required">
+                  <input 
+                    className="input100" 
                     type="email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.onChange}
+			              name="email"
+			              value={this.state.email}
+			              onChange={this.onChange}
                   />
-                  <span className=""/>
-                </div>
+						      <span className="focus-input100" data-placeholder="Email"></span>
+					      </div>
 
-                <div className="">
-                  <span className="">Password</span>
-                  <br />
-                  <a href="/forgotPassword" className="">
-                    Forgot?
-                  </a>
-                </div>
+					<div className="wrap-input100 validate-input" data-validate="Password is required">
+						<span className="btn-show-pass">
+							<i className="zmdi zmdi-eye"></i>
+						</span>
+            <input 
+              className="input100"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+						<span className="focus-input100" data-placeholder="Password"></span>
+					</div>
 
-                <div className="" data-validate="Password is required">
-                  <input
-                    className=""
-                    type="password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.onChange}
-                  />
-                  <span className="" />
-                </div>
+					<div className="container-login100-form-btn">
+						<div className="wrap-login100-form-btn">
+							<div className="login100-form-bgbtn"></div>
+							<button className="login100-form-btn" type="submit">
+								Login
+							</button>
+						</div>
+					</div>
 
-                <div className="">
-                  <button type="submit" className="">
-                    Sign In
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+					<div className="text-center p-t-115">
+						<span className="txt1">
+							Don’t have an account?
+						</span>
+
+						<a className="txt2" href="/forgotPassword">
+							Sign Up
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/login';
+import "./css/font.css";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
